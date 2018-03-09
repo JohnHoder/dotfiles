7322c8e26e65d60bb85f64833b35824d8a1efa90
JohnHoder <hoder.john@gmail.com>
2018-03-09 20:48:08 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ set -e
 git clone --recursive https://github.com/JohnHoder/dotfiles.git
 cd dotfiles && git remote 'set-url' --push origin git@github.com:JohnHoder/dotfiles.git
 
-exec ./setup.sh
+exec ./setupansible.sh
+exec ./setupdotfiles.sh
 
