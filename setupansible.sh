67b7cf82b7266e3681b596f759685a9a667afe0a
JohnHoder <hoder.john@gmail.com>
2019-08-08 03:52:44 +0200

    Zsh plugins setup fix

--- setupansible.sh
+++ setupansible.sh
@@ -6,7 +6,7 @@ echo "Setting up system with Ansible as [root]"
 ansible-playbook -v -i "localhost," -c local ansible_playbooks/ansible_root.yml
 
 echo "Setting up system with Ansible as [user]"
-ansible-playbook -v -i "localhost," -c local ansible_playbooks/ansible_user.yml
+ansible-playbook -vvv -i "localhost," -c local ansible_playbooks/ansible_user.yml
 
 echo "Ansible - DONE"
 
