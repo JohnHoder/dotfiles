bcfe391d38d346fc7c4fa4a5f9c0ce0849e79414
JohnHoder <hoder.john@gmail.com>
2020-10-24 20:22:38 +0200

    conky: weather; pyown changed to APIv3, provisoire fix

--- conky/rightpanel/conky_weather/weather_parser.py
+++ conky/rightpanel/conky_weather/weather_parser.py
@@ -4,37 +4,42 @@ import os
 import pyowm
 import urllib.request
 import datetime
+from pyowm.utils.config import get_default_config
+
+config_dict = get_default_config()
+config_dict['language'] = 'ru'
 
 times = datetime.datetime.now()
 
 API_key = 'f68237332758a41ef7a6e1e737175dde'    # API-key https://openweathermap.org/
-owm = pyowm.OWM(API_key, language='ru')
+owm = pyowm.OWM(API_key, config_dict)
 
 sity = 'Гавиржов'  # you location sity
-observation = owm.weather_at_place(sity)
-w = observation.get_weather()
-temperature = w.get_temperature('celsius')['temp']
-observation_list = owm.weather_around_coords(-22.57, -43.12)
-atmospheric_pressure = w.get_pressure()['press']
-humidity_percentage = w.get_humidity()
-speeed = w.get_wind()['speed']
-cloud = w.get_clouds()
-znak = w.get_weather_icon_url()
-icon_znak = urllib.request.urlopen(znak)
+mgr = owm.weather_manager()
+observation = mgr.weather_at_place(sity)
+w = observation.weather
+temperature = w.temperature('celsius')['temp']
+#observation_list = owm.weather_around_coords(-22.57, -43.12)
+#atmospheric_pressure = w.pressure()
+humidity_percentage = w.wind().get('humidity', 0)
+speed = w.wind().get('speed', 0)
+#cloud = w.clouds()
+#znak = w.get_weather_icon_url()
+#icon_znak = urllib.request.urlopen(znak)
 
 selfpath = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 txtfilepath = os.path.join(selfpath, 'weather.txt')
-iconfilepath = os.path.join(selfpath, '../images/icons_weather.png')
+#iconfilepath = os.path.join(selfpath, '../images/icons_weather.png')
 
-file = open(iconfilepath, 'wb')
-file.write(icon_znak.read())
-file.close()
+#file = open(iconfilepath, 'wb')
+#file.write(icon_znak.read())
+#file.close()
 
 file = open(txtfilepath, 'w')     # path to file
 file.write('На ' + times.strftime("%d-%m-%Y %H:%M") + '\n')
 file.write('В ' + sity + 'е наблюдается:\n')
-file.write(w.get_detailed_status() + '. Влажноcть ' + str(humidity_percentage) + '%')
+#file.write(w.get_detailed_status() + '. Влажноcть ' + str(humidity_percentage) + '%')
 file.write('\nТемпература воздуха: ' + str(temperature) + '℃')
-file.write('\nДавление, мм рт.ст: ' + str(atmospheric_pressure) + ' hpa')
-file.write('\nСкорость ветра: ' + str(speeed) + 'м\с')
+#file.write('\nДавление, мм рт.ст: ' + str(atmospheric_pressure) + ' hpa')
+file.write('\nСкорость ветра: ' + str(speed) + 'м\с')
 file.close()
