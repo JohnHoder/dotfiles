4b9af3b338dbe4f5110e668e2b20614974371122
JohnHoder <hoder.john@gmail.com>
2020-10-24 18:24:46 +0200

    i3: automatic monitor configuration update

--- i3/autoxrandr.sh
+++ i3/autoxrandr.sh
@@ -3,28 +3,34 @@
 if [[ $(xrandr | grep '\Wconnected' | awk '{ print $1 }' | wc -l) == "1" ]]; then
     xrandr --output HDMI1 --off
     xrandr --output VGA1 --off
-    if [[ $(ps -o etimes= -p "`pgrep -o -x conky`") -gt 7 ]]; then
-        killall -SIGUSR1 conky
-    fi
-    #killall -SIGUSR1 conky
-    echo yessir
+    echo "Single monitor mode"
 fi
+
+# Set lvds variable
 for output in $(xrandr | grep '\Wconnected' | awk '{ print $1 }'); do
     if [[ $output =~ ^LVDS.*$ ]]; then
         lvds=$output
-        #killall -SIGUSR1 conky
-        if [[ $(ps -o etimes= -p "`pgrep -o -x conky`") -gt 7 ]]; then
-            killall -SIGUSR1 conky
-        fi
     fi
 done
+
+# Set-up all the monitors
+counter=0
 for output in $(xrandr | grep '\Wconnected' | awk '{ print $1 }'); do
     if [[ ! $output =~ ^LVDS.*$ ]]; then
-        #xrandr --output $lvds --auto --output $output --pos 0x0 --auto --right-of $lvds --primary
-        xrandr --output $output --auto --left-of $lvds
-        #killall -SIGUSR1 conky
-        if [[ $(ps -o etimes= -p "`pgrep -o -x conky`") -gt 7 ]]; then
-            killall -SIGUSR1 conky
-        fi
+        echo $output
+        if [ "${counter}" -eq "0" ]; then
+            #echo $counter
+            #xrandr --output $lvds --auto --output $output --pos 0x0 --auto --right-of $lvds --primary
+            xrandr --output $output --auto --left-of $lvds --primary
+        elif [ "${counter}" -eq "1" ]; then
+            xrandr --output $output --auto --right-of $lvds
+            #echo $counter
+	fi
+        counter=$((counter+1))
     fi
 done
+
+#killall -SIGUSR1 conky
+if [[ $(ps -o etimes= -p "`pgrep -o -x conky`") -gt 7 ]]; then
+    killall -SIGUSR1 conky
+fi
