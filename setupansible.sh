4fafe2c5f446204e6c08ee64bc364fd1423bfbb5
JohnHoder <hoder.john@gmail.com>
2022-04-09 03:25:10 +0200

    Ansible: cleanup

--- setupansible.sh
+++ setupansible.sh
@@ -5,11 +5,15 @@
 echo "Setting up system with Ansible as [root]"
 ansible-playbook -v -i "localhost," -c local ansible_playbooks/ansible_root.yml
 
-echo "Setting up TeX packages and texmaker"
-ansible-playbook -v -i "localhost," -c local ansible_playbooks/ansible_root_texmaker.yml
+#echo "Setting up TeX packages and texmaker"
+#ansible-playbook -v -i "localhost," -c local ansible_playbooks/ansible_root_texmaker.yml
 
 echo "Setting up system with Ansible as [user]"
-ansible-playbook -vvv -i "localhost," -c local ansible_playbooks/ansible_user.yml
+ansible-playbook -v -i "localhost," -c local ansible_playbooks/ansible_user.yml
 
 echo "Ansible - DONE"
+
+### MORE VERBOSE ###
+# Use -vvv
+
 
